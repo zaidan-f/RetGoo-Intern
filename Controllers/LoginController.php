7ec409ab4b3d629c8f0bcb2b76cf9fc31448cdcb
Zaidan F.A <98175556+zaidan-f@users.noreply.github.com>
2023-03-23 20:55:44 +0700

    controller

---
+++ Controllers/LoginController.php
@@ -0,0 +1,44 @@
+<?php
+ 
+namespace App\Http\Controllers;
+ 
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+ 
+class LoginController extends Controller
+{
+    public function index()
+    {
+        if (Auth::check()) {
+            return redirect('/landing');
+        }else{
+            return view('login');
+        }
+    }
+ 
+    public function authenticate(Request $request)
+    {
+        $credentials = $request->validate([
+            'email' => ['required' ],
+            'password' => ['required']
+        ]);
+ 
+        if (Auth::attempt($credentials)) {
+            $request->session()->regenerate();
+            return redirect()->intended('/landing');
+        }
+ 
+        return back()->with('loginError', 'Login failed');
+    }
+ 
+    public function logout(Request $request)
+    {
+        Auth::logout();
+ 
+        request()->session()->invalidate();
+ 
+        request()->session()->regenerateToken();
+ 
+        return redirect('login');
+    }
+}
