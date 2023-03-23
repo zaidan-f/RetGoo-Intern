5869779fd9576699921882f1539ca40523aec8ea
Zaidan F.A <98175556+zaidan-f@users.noreply.github.com>
2023-03-23 20:56:22 +0700

    route keren

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,11 @@ use App\Http\Controllers\HomeController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\LandingController;
+use App\Http\Controllers\FormController;
+use App\Http\Controllers\AfterController;
+
+
  
 Route::get('/register', [RegisterController::class, 'index']);
 Route::post('/register', [RegisterController::class, 'store']);
@@ -12,4 +17,9 @@ Route::get('/login', [LoginController::class, 'index'])->name('login')->middlewa
 Route::post('/login', [LoginController::class, 'authenticate']);
 Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard')->middleware('auth');
 Route::post('/logout', [LoginController::class, 'logout']);
-Route::get('/', [HomeController::class, 'index'])->name('home')->middleware('auth');
+Route::get('/', [LandingController::class, 'index'])->name('landing')->middleware('auth');
+Route::get('/landing', [LandingController::class, 'index']);
+Route::get('/form', [FormController::class, 'index']);
+Route::post('/form', [FormController::class, 'index']);
+
+Route::get('/after', [AfterController::class, 'index']);
