69cc11c346e163a3892da077e6d44122ba919560
Zaidan F.A <98175556+zaidan-f@users.noreply.github.com>
2023-04-10 10:27:42 +0700

    public update

--- public/js/app.js
+++ public/js/app.js
@@ -4,3 +4,12 @@ checkbox.addEventListener("change", () => {
   // Toggle website theme
   document.body.classList.toggle("dark");
 });
+
+window.onscroll = function() {myFunction()};
+
+function myFunction() {
+  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  var height = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+  var scrolled = (winScroll / height) * 100;
+  document.getElementById("myBar").style.width = scrolled + "%";
+}
