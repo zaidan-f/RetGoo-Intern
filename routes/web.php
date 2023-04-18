4d8161260ef5d754bbb341bddd8b0e9d1430fa4d
Zaidan F.A <98175556+zaidan-f@users.noreply.github.com>
2023-04-18 12:22:06 +0700

    update web routes

--- routes/web.php
+++ routes/web.php
@@ -7,19 +7,47 @@ use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\LandingController;
 use App\Http\Controllers\FormController;
 use App\Http\Controllers\AfterController;
+use App\Http\Controllers\AccountController;
+use App\Http\Controllers\testcontroller;
+use App\Http\Controllers\ImageController;
+use App\Http\Controllers\ProfileController;
 
 
- 
+// register
 Route::get('/register', [RegisterController::class, 'index']);
 Route::post('/register', [RegisterController::class, 'store']);
 
+// login
 Route::get('/login', [LoginController::class, 'index'])->name('login')->middleware('guest');
 Route::post('/login', [LoginController::class, 'authenticate']);
+
+// dashboard
 Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard')->middleware('auth');
-Route::post('/logout', [LoginController::class, 'logout']);
+
+// logout
+Route::get('/logout', [LoginController::class, 'logout']);
+
+// landing page
 Route::get('/', [LandingController::class, 'index'])->name('landing')->middleware('auth');
 Route::get('/landing', [LandingController::class, 'index']);
+
+// form
 Route::get('/form', [FormController::class, 'index']);
 Route::post('/form', [FormController::class, 'index']);
 
+// after submit
 Route::get('/after', [AfterController::class, 'index']);
+
+// account page
+Route::get('/account', [AccountController::class, 'index']);
+
+// test 
+Route::get('/test', [testcontroller::class, 'index']);
+
+// image 
+Route::get('image-upload', [ ImageController::class, 'index' ]);
+Route::post('image-upload', [ ImageController::class, 'store' ])->name('image.store');
+
+// profile pic
+Route::get('/profile', [ProfileController::class, 'index'])->name('user.profile');
+Route::post('/profile', [ProfileController::class, 'store'])->name('user.profile.store');
