f710dbdb4080cf024819441edef6779fd486feb7
Zaidan F.A <98175556+zaidan-f@users.noreply.github.com>
2023-06-13 13:53:30 +0700

    update feature
    update views:
    account.blade.php : Add banner and avatar 
    profile.blade.php : Upload page for avatars and banner image

--- resources/views/account.blade.php
+++ resources/views/account.blade.php
@@ -1,83 +1,85 @@
 <!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <title>{{ auth()->user()->name ?? '' }}</title>
-        <link rel="stylesheet" type="text/css" href="/css/account.css">    
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
-        <link rel="manifest" href="/site.webmanifest">
-    
-        <title>Account Page</title>
+  <html lang="en">
+      <head>
+          <meta charset="UTF-8">
+          <meta name="viewport" content="width=device-width, initial-scale=1.0">
+          <meta http-equiv="X-UA-Compatible" content="ie=edge">
+          <title>{{ auth()->user()->name ?? '' }}</title>
+          <link rel="stylesheet" type="text/css" href="/css/account.css">    
+          <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+          <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+          <link rel="manifest" href="/site.webmanifest">
+          <title>Account Page</title>
       </head>
-<body>
-    <nav>
-        <span><img class="logo" src="logo.png" alt="" width="20%"></span>
-        <a href="/" class="logo">
-          <h3>
-            RetGoo <br> Internship System
-          </h3>
-        </a>
-        <div class="theme-switch">
-            <input type="checkbox" class="checkbox" id="checkbox" />
-            <label for="checkbox" class="label">
-               <ion-icon name="partly-sunny-outline" class="sun"></ion-icon>
-               <ion-icon name="moon-outline" class="moon"></ion-icon>
-               <div class="switcher"></div>
-            </label>
-        </div>
-        <img class="navimg" src="/avatars/{{ Auth::user()->avatar }}">
-        <a href="/account">
-          <div class="nama">
-          <p>{{ auth()->user()->name ?? '' }}</p>
-        </div>
-      </a>
-    </nav>
+      
+      <body>
+          <nav>
+              <span><img class="logo" src="logo.png" alt="" width="20%"></span>
+              <a href="/" class="logo">
+                <h3>
+                  RetGoo <br> Internship System
+                </h3>
+              </a>
+              <div class="theme-switch">
+                  <input type="checkbox" class="checkbox" id="checkbox" />
+                  <label for="checkbox" class="label">
+                    <ion-icon name="partly-sunny-outline" class="sun"></ion-icon>
+                    <ion-icon name="moon-outline" class="moon"></ion-icon>
+                    <div class="switcher"></div>
+                  </label>
+              </div>
+              <div class="line"></div>
+              <img class="navimg" src="/avatars/{{ Auth::user()->avatar }}">
+              <a href="/account">
+                <div class="nama">
+                <p>{{ auth()->user()->name ?? '' }}</p>
+              </div>
+            </a>
+          </nav>
+ 
+          <section class="banner">
+            <img class="accbanner" src="/banners/{{ auth()->user()->banner }}" alt="">
+        </section>
 
-    <section class="banner">
-    </section>
-
-    <section class="profile">
-      <img class="accimg" src="/avatars/{{ auth::user()->avatar }}" alt="">
-    <div class="namaprofil">
-      <h1>
-        <span class="name">{{ auth()->user()->name ?? '' }}</span>
-      </h1>
-      <a href="/profile"><Button class="btn" formaction="/profile">Edit Profil<ion-icon name="pencil-sharp" class="pencil"></ion-icon></Button></a>
-    </div>
-    </section>
-
-    <section class="details">
-    <div class="data">
-      <p><ion-icon name="mail" class="icondt"></ion-icon>{{ auth()->user()->email ?? '' }}</p><br>
-      <p><ion-icon name="business" class="icondt"></ion-icon>{{ auth()->user()->sekolah ?? '' }} </p><br>
-      <p><ion-icon name="laptop-outline" class="icondt"></ion-icon>{{ auth()->user()->jurusan ?? '' }}</p><br>
-      <p><ion-icon name="calendar-sharp" class="icondt"></ion-icon>{{ auth()->user()->tgl_mulai ?? '' }}  Sampai  {{ auth()->user()->tgl_selesai ?? '' }}</p><br>
-      <p><ion-icon name="home" class="icondt"></ion-icon>{{ auth()->user()->alamat ?? '' }}</p><br>
-      <p><ion-icon name="call" class="icondt"></ion-icon>{{ auth()->user()->telpon ?? '' }}</p><br>
-    </div>
-    </section>
-
-      <footer id="footer">
-          <div class="container py-4">
-            <div class="copyright">
-              &copy; Copyright <strong><span>RetGoo</span></strong>. All Rights Reserved
-            </div>
-            <div class="credits">
-              Designed by Zaidan</a>
-            </div>
+          <section class="profile">
+            <img class="accimg" src="/avatars/{{ auth::user()->avatar }}" alt="">
+          <div class="namaprofil">
+            <h1>
+              <span class="name">{{ auth()->user()->name ?? '' }}</span>
+            </h1>
+            <a href="/profile"><Button class="btn" formaction="/profile">Edit Profil<ion-icon name="pencil-sharp" class="pencil"></ion-icon></Button></a>
           </div>
-      </footer>
-    
+          </section>
 
-      <script src="/js/app.js"></script>
-    
-      <script
-              type="module"
-              src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js">
-      </script>
-</body>
-</html>
+          <section class="details">
+          <div class="data">
+            <p><ion-icon name="mail" class="icondt"></ion-icon>{{ auth()->user()->email ?? '' }}</p><br>
+            <p><ion-icon name="business" class="icondt"></ion-icon>{{ auth()->user()->sekolah ?? '' }} </p><br>
+            <p><ion-icon name="laptop-outline" class="icondt"></ion-icon>{{ auth()->user()->jurusan ?? '' }}</p><br>
+            <p><ion-icon name="calendar-sharp" class="icondt"></ion-icon>{{ auth()->user()->tgl_mulai ?? '' }}  Sampai  {{ auth()->user()->tgl_selesai ?? '' }}</p><br>
+            <p><ion-icon name="home" class="icondt"></ion-icon>{{ auth()->user()->alamat ?? '' }}</p><br>
+            <p><ion-icon name="call" class="icondt"></ion-icon>{{ auth()->user()->telpon ?? '' }}</p><br>
+            <form action="/logout"><button class="logout">Log Out  <ion-icon name="exit-sharp" class="exit"></ion-icon></button></form>
+          </div>
+          </section>
+          
+            <footer id="footer">
+                <div class="container py-4">
+                  <div class="copyright">
+                    &copy; Copyright <strong><span>RetGoo</span></strong>. All Rights Reserved
+                  </div>
+                  <div class="credits">
+                    Designed by Zaidan</a>
+                  </div>
+                </div>
+            </footer>
+
+            <script src="/js/app.js"></script>
+          
+            <script
+                    type="module"
+                    src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js">
+            </script>
+      </body>
+  </html>
--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -3,7 +3,7 @@
 <head>
     <title>Upload Image</title>
     <link rel="stylesheet" type="text/css" href="/css/profile.css">
-</head>
+</head> 
       
 <body>
     <div class="container">
@@ -21,16 +21,46 @@
                                     {{ session('success') }}
                                 </div>
                             @endif
-      
+
+                            <!-- Avatar Upload -->
+                            @if (Auth::user()->avatar)
+                                <div class="row mb-3">
+                                    <label class="col-md-4 col-form-label text-md-end">{{ __('Current Avatar') }}</label>
+                                    <div class="col-md-6">
+                                        <img src="/avatars/{{ Auth::user()->avatar }}" style="width:80px;margin-top: 10px;">
+                                    </div>
+                                </div>
+                            @endif
+
                             <div class="row mb-3">
-                                <label for="avatar" class="col-md-4 col-form-label text-md-end">{{ __('Avatar') }}</label>
-      
+                                <label for="avatar" class="col-md-4 col-form-label text-md-end">{{ __('New Avatar') }}</label>
                                 <div class="col-md-6">
                                     <input id="avatar" type="file" class="form-control @error('avatar') is-invalid @enderror" name="avatar" value="{{ old('avatar') }}" required autocomplete="avatar">
       
-                                    <img src="/avatars/{{ Auth::user()->avatar }}" style="width:80px;margin-top: 10px;">
-      
                                     @error('avatar')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                    @enderror
+                                </div>
+                            </div>
+
+                            <!-- Banner Upload -->
+                            @if (Auth::user()->banner)
+                                <div class="row mb-3">
+                                    <label class="col-md-4 col-form-label text-md-end">{{ __('Current Banner') }}</label>
+                                    <div class="col-md-6">
+                                        <img src="/banners/{{ Auth::user()->banner }}" style="width:200px;margin-top: 10px;">
+                                    </div>
+                                </div>
+                            @endif
+
+                            <div class="row mb-3">
+                                <label for="banner" class="col-md-4 col-form-label text-md-end">{{ __('New Banner') }}</label>
+                                <div class="col-md-6">
+                                    <input id="banner" type="file" class="form-control @error('banner') is-invalid @enderror" name="banner" value="{{ old('banner') }}" required autocomplete="banner">
+      
+                                    @error('banner')
                                         <span class="invalid-feedback" role="alert">
                                             <strong>{{ $message }}</strong>
                                         </span>
@@ -40,15 +70,32 @@
       
                             <div class="submit">
                                 <div class="col-md-8 offset-md-4">
-                                    <button type="submit" class="btn-up">
-                                        {{ __('Upload Profile') }}
-                                    </button>
+                                    <button type="submit" class="btn-up">{{ __('Upload Profile') }}</button>
                                 </div>
                             </div>
                         </form>
+
+                        <!-- Avatar Deletion -->
+                        <div class="delete">
+                            <form method="POST" action="{{ route('user.profile.destroy') }}">
+                                @csrf
+                                @method('DELETE')
+                                <button type="submit" class="btndel">Delete Avatar</button>
+                            </form>
+                        </div>
+
+                        <!-- Banner Deletion -->
+                        <div class="delete">
+                            <form method="POST" action="{{ route('user.profile.destroy.banner') }}">
+                                @csrf
+                                @method('DELETE')
+                                <button type="submit" class="btndel">Delete Banner</button>
+                            </form>
+                        </div>
+
                         <form action="/account" >
                             <div class="next">
-                            <button class="btnnext" type="submit">Next</button>
+                                <button class="btnnext" type="submit">Next</button>
                             </div>
                         </form>
                     </div>
